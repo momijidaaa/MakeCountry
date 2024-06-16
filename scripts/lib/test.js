5f68b9d40dc1bf6dc2ddced0d601f98ecccd3b8d
karonyt <tomatotest34@gmail.com>
2024-06-16 13:28:22 +0900

    feat: create test.js

---
+++ scripts/lib/test.js
@@ -0,0 +1,25 @@
+import { Player, ScriptEventSource, system } from "@minecraft/server";
+import { GetAndParsePropertyData, StringifyAndSavePropertyData } from "./util";
+
+system.afterEvents.scriptEventReceive.subscribe((ev)=>{
+    if(ev.sourceType !== ScriptEventSource.Entity || !(ev.sourceEntity instanceof Player)) return;
+    const { sourceEntity,message } = ev;
+    const playerData = GetAndParsePropertyData(`player_${sourceEntity.id}`);
+    switch(ev.id) {
+        case `karo:add`: {
+            playerData.money += Number(message);
+            StringifyAndSavePropertyData(`player_${sourceEntity.id}`,playerData);
+            break;
+        };
+        case `karo:remove`: {
+            playerData.money -= Number(message);
+            StringifyAndSavePropertyData(`player_${sourceEntity.id}`,playerData);
+            break;
+        };
+        case `karo:set`: {
+            playerData.money = Number(message);
+            StringifyAndSavePropertyData(`player_${sourceEntity.id}`,playerData);
+            break;
+        };
+    };
+});
