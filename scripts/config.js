eb4774d7e148b4053ffbb0903a76361521c4dfb7
karonyt <tomatotest34@gmail.com>
2024-06-16 17:37:20 +0900

    update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -14,7 +14,7 @@ export default {
     //国際組織の設立にかかる金
     MakeInternationalOrganizationCost: 100000,
     //共通通貨の単位
-    MoneyName: `￥`,
+    MoneyName: `$`,
     //初期の国庫の金
     initialCountryMoney: 0,
     //初期のリソースポイント
