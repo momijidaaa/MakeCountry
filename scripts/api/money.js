f74a794f9e79e63198e306394ce655ac2f600a70
Number-Block <numberblockserver@outlook.jp>
2024-09-18 20:56:31 +0900

    エラーハンドリングを追加

--- scripts/api/money.js
+++ scripts/api/money.js
@@ -4,27 +4,44 @@ import { GetAndParsePropertyData, StringifyAndSavePropertyData } from "../lib/ut
 
 system.afterEvents.scriptEventReceive.subscribe((ev) => {
     const { id, sourceEntity, message } = ev;
+
     switch (id) {
         case `mcapi:moneyadd`: {
             const playerData = GetAndParsePropertyData(`player_${sourceEntity.id}`);
-            playerData.money += parseInt(message);
+            if (playerData === undefined) return;
+            const newMoney = parseInt(message);
+            if (isNaN(newMoney)) return;
+            if ((playerData.money + newMoney) < 0) return;
+
+            playerData.money += newMoney;
             StringifyAndSavePropertyData(`player_${sourceEntity.id}`, playerData);
             break;
         };
         case `mcapi:moneyremove`: {
             const playerData = GetAndParsePropertyData(`player_${sourceEntity.id}`);
-            playerData.money -= parseInt(message);
+            if (playerData === undefined) return;
+            const newMoney = parseInt(message);
+            if (isNaN(newMoney)) return;
+            if ((playerData.money - newMoney) < 0) return;
+
+            playerData.money -= newMoney;
             StringifyAndSavePropertyData(`player_${sourceEntity.id}`, playerData);
             break;
         };
         case `mcapi:moneyset`: {
             const playerData = GetAndParsePropertyData(`player_${sourceEntity.id}`);
-            playerData.money = parseInt(message);
+            if (playerData === undefined) return;
+            const newMoney = parseInt(message);
+            if (isNaN(newMoney)) return;
+            if (newMoney < 0) return;
+
+            playerData.money = newMoney;
             StringifyAndSavePropertyData(`player_${sourceEntity.id}`, playerData);
             break;
         };
         case `mcapi:moneyget`: {
             const playerData = GetAndParsePropertyData(`player_${sourceEntity.id}`);
+            if (playerData === undefined) return;
             playerData.money;
             sendData(`mcapi:moneygetresult`, `result`, playerData.money, message);
             break;
