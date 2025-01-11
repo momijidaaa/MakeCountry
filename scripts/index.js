38461a22ce120b3f4a1f80b05d9ed306760cf59f
karonyt <tomatotest34@gmail.com>
2025-01-11 19:21:37 +0900

    update manifest.json and scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -24,44 +24,11 @@ import "./lib/chest_shop";
 
 import "./lib/ranking";
 
-import { sendToDiscord } from "./lib/server_net";
-
 import "./lib/scriptcommand";
 
 //import "./lib/piston";
 
 const version = "ver.1.21.50.β.1.1.0";
-
-
-world.afterEvents.entitySpawn.subscribe((ev) => {
-    if (ev.entity.typeId == "minecraft:item") {
-        const item = ev.entity.getComponent(`item`)?.itemStack;
-        if(!item) return;
-        if (item.typeId == "minecraft:diamond_block" || item.typeId == "minecraft:netherite_block") {
-            if (item.amount > 0) {
-                let names = [];
-                const nearPlayers = ev.entity.dimension.getPlayers({ maxDistance: 100, location: ev.entity.location });
-                for(const pn of nearPlayers) {
-                    names.push(`${pn.name}(${pn.id})[${JSON.stringify(pn.location)}]`);
-                };
-                world.getPlayers({ tags: [`mc_admin`, `moderator`] }).forEach(p => {
-                    p.sendMessage(`${JSON.stringify(ev.entity.location)} で ${item.typeId} が ${item.amount} 出現\n半径100m以内のプレイヤー${names.join(`\n`)}`);
-                });
-                sendToDiscord({
-                    channelId: `1324622173582655558`,
-                    content: {
-                        embeds: [
-                            {
-                                color: 0x7cfc00,
-                                description: `${JSON.stringify(ev.entity.location)} で ${item.typeId} が ${item.amount} 出現\n半径100m以内のプレイヤー${names.join(`\n`)}`
-                            }
-                        ]
-                    }
-                })
-            };
-        };
-    };
-});
 
 world.afterEvents.worldInitialize.subscribe((ev) => {
     world.sendMessage({ translate: `world.message.addon`, with: [version] });
