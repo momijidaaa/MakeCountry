8fd5709d9874d89be8bde34aa2afe3fc7cff01d8
karonyt <tomatotest34@gmail.com>
2025-06-27 14:28:35 +0900

    fix

--- scripts/lib/app/karcari.js
+++ scripts/lib/app/karcari.js
@@ -10,6 +10,7 @@ import { ActionForm, ModalForm } from "../form_class";
 const ActionFormData = ActionForm;
 const ModalFormData = ModalForm;
 import { itemIdToPath } from "../../texture_config";
+import { typeIdToID } from "../typeIds";
 
 /**
  * @type {DynamicProperties}
@@ -71,7 +72,7 @@ export function KarcariApp(player) {
  * 売却履歴
  * @param {Player} player 
  */
-export function PlayerMarketCheckSold(player) {
+function PlayerMarketCheckSold(player) {
     const playerData = GetAndParsePropertyData(`player_${player.id}`);
     /**
      * @type {Array<{ price: number, user: string, item: {name: undefined|string, typeId: string,amount: number,lore: [string] } }>}
@@ -110,7 +111,7 @@ export function PlayerMarketCheckSold(player) {
  * 出品したアイテム一覧
  * @param {Player} player 
  */
-export function PlayerMarketWithdrawalGoodsMainMenu(player) {
+function PlayerMarketWithdrawalGoodsMainMenu(player) {
     const form = new ActionFormData();
     form.title(`Karcari`)
     form.button({ translate: `mc.button.close` });
@@ -205,7 +206,7 @@ export function PlayerMarketWithdrawalGoodsSelectMenu(player, common) {
  * 出品アイテムの選択
  * @param {Player} player 
  */
-export function PlayerMarketExhibitMainMenu(player) {
+function PlayerMarketExhibitMainMenu(player) {
     const data = GetAndParsePropertyData(`player_market_commons`) ?? [];
     /**
  * @type {Array<{id: number,playerName: string,playerId: string,price: number, item: {name: undefined|string,typeId: string,amount: number}}>}
@@ -268,7 +269,7 @@ export function PlayerMarketExhibitMainMenu(player) {
  * @param {ItemStack} itemStack 
  * @param {Player} player 
  */
-export function PlayerMarketExhibitSelectItemMenu(player, itemData, itemStack) {
+function PlayerMarketExhibitSelectItemMenu(player, itemData, itemStack) {
     const form = new ModalFormData();
     form.title(`Karcari`);
     form.slider(``, 1, itemData.itemStack.amount, 1);
